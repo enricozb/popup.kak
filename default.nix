388d7e5a3fb69099dbfcdac7fe01c1733986c1f3
Enrico Zandomeni Borba <enricozb@gmail.com>
2023-06-07 17:36:05 -0400

    bump version

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "kak-popup";
-  version = "0.4.0-alpha.2";
+  version = "0.4.0-alpha.3";
   src = ./.;
 
   cargoLock = { lockFile = ./Cargo.lock; };
