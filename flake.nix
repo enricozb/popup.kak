6a821f57dcfe0c1c0ce07c9d596bf64ffcb5d689
Enrico Borba <enricozb@users.noreply.github.com>
2024-04-29 15:13:08 +0200

    fmt flake.nix

--- flake.nix
+++ flake.nix
@@ -3,24 +3,20 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      kak-popup = pkgs.callPackage ./default.nix {};
-    in {
-      packages = {
-        default = kak-popup;
-        kak-popup = kak-popup;
-      };
+  outputs = { self, nixpkgs, flake-utils, }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        kak-popup = pkgs.callPackage ./default.nix { };
+      in {
+        packages = {
+          default = kak-popup;
+          kak-popup = kak-popup;
+        };
 
-      devShells.default =
-        pkgs.mkShell {packages = with pkgs; [cargo clippy rustc tmux];};
-    })
-    // {
-      overlays.default = import ./overlay.nix;
-    };
+        devShells.default =
+          pkgs.mkShell { packages = with pkgs; [ cargo clippy rustc tmux ]; };
+      }) // {
+        overlays.default = import ./overlay.nix;
+      };
 }
