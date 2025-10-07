20962505840877d445c4b95a580c01e8fc8bfc6a
Enrico Zandomeni Borba <enricozb@gmail.com>
2025-10-07 07:34:37 +0200

    include rc/ in nix package

--- default.nix
+++ default.nix
@@ -2,8 +2,13 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "kak-popup";
-  version = "0.6.2-beta";
+  version = "0.6.3-beta";
   src = ./.;
 
   cargoLock = { lockFile = ./Cargo.lock; };
+
+  postInstall = ''
+    mkdir -p $out/share/kak/autoload/plugins/
+    cp -r ${./rc} $out/share/kak/autoload/plugins/popup.kak/
+  '';
 }
